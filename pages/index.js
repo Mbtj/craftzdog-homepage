2fec1172118ace82036a36917b219319db5f3128
TJ <michael.btj@hotmail.com>
2022-04-06 21:26:26 -0400

    change location :o

--- pages/index.js
+++ pages/index.js
@@ -36,7 +36,7 @@ const Home = () => (
         textAlign="center"
         bg={useColorModeValue('whiteAlpha.500', 'whiteAlpha.200')}
       >
-        Hello, I&apos;m a full-stack developer based in Japan!
+        Hello, I&apos;m a full-stack developer based in Toronto, Ontario!
       </Box>
 
       <Box display={{ md: 'flex' }}>
